cebdf2ebdea905715367783ce3d12af659459d85
Valentin Sergeev <galahed1205@gmail.com>
2022-08-29 12:08:00 +0300

    Added few text blocks

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,8 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
-// void main() {
-//   //runApp(const MyApp());
-//   runApp(MaterialApp(
-//     home: Scaffold(
-//         backgroundColor: Colors.indigo,
-//         appBar: AppBar(
-//           title: const Text("My third flutter App"),
-//           centerTitle: true,
-//         ),
-//         body: const Center(
-//           child: Text(
-//             "This is My Homepage",
-//             style: TextStyle(
-//                 fontSize: 30,
-//                 fontWeight: FontWeight.bold,
-//                 color: Colors.white),
-//           ),
-//         )),
-//   ));
-// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -28,16 +10,98 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Counter',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: CounterWidget(
-          title: 'Counter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Weather Forecast',
+            style: TextStyle(color: Colors.white),
+          ),
           centerTitle: true,
-        ));
+          backgroundColor: Colors.red.shade400,
+        ),
+        body: _buildBody(),
+        backgroundColor: Colors.red.shade400,
+      ),
+    );
   }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                size: 20,
+                color: Colors.white,
+              ),
+            ),
+            const Expanded(
+                child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter City Name',
+                labelStyle: TextStyle(color: Colors.white),
+                border: InputBorder.none,
+              ),
+            )),
+          ],
+        ),
+        Divider(
+          color: Colors.red.shade400,
+        ),
+        const Text(
+          'Murmansk Oblast, RU',
+          style: TextStyle(fontSize: 25.0, color: Colors.white),
+        ),
+        Divider(
+          color: Colors.red.shade400,
+        ),
+        const Text(
+          'Friday, May 20, 2020',
+          style: TextStyle(fontSize: 16.0, color: Colors.white),
+        ),
+        Divider(
+          height: 30.0,
+          color: Colors.red.shade400,
+        ),
+        _weatherDescription(),
+      ],
+    ),
+  );
+}
+
+Row _weatherDescription() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.sunny,
+            color: Colors.white,
+            size: 60.0,
+          )),
+      const SizedBox(width: 40.0),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            '14°F',
+            style: TextStyle(fontSize: 30, color: Colors.white),
+          ),
+          Text(
+            'LIGHT SNOW',
+            style: TextStyle(fontSize: 16, color: Colors.white),
+          )
+        ],
+      )
+    ],
+  );
 }
 
 class CounterWidget extends StatefulWidget {
@@ -85,7 +149,8 @@ class _CounterWidgetState extends State<CounterWidget> {
               ),
               Container(
                 width: double.infinity,
-                margin: const EdgeInsets.symmetric(horizontal: 130.0, vertical: 5),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 130.0, vertical: 5),
                 decoration: BoxDecoration(
                   color: Colors.indigo[50],
                   borderRadius: BorderRadius.circular(10),
@@ -103,7 +168,6 @@ class _CounterWidgetState extends State<CounterWidget> {
                   ],
                 ),
               ),
-
               const Text(
                 'Tap "+" to increment',
                 style: TextStyle(fontSize: 14, color: Colors.white),
@@ -115,113 +179,3 @@ class _CounterWidgetState extends State<CounterWidget> {
     );
   }
 }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // Try running your application with "flutter run". You'll see the
-//         // application has a blue toolbar. Then, without quitting the app, try
-//         // changing the primarySwatch below to Colors.green and then invoke
-//         // "hot reload" (press "r" in the console where you ran "flutter run",
-//         // or simply save your changes to "hot reload" in a Flutter IDE).
-//         // Notice that the counter didn't reset back to zero; the application
-//         // is not restarted.
-//         primarySwatch: Colors.indigo,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-//
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-//
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-//
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-//
-//   final String title;
-//
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-//
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
